7f8b37b940f341b619ad159626e3284d164e1669
Alba Gutierrez-Sacristan <alba.gutierrez@upf.edu>
2016-04-18 12:18:24 +0000

    Solved problems raised on DisGeNET updated. Updated documentation of the package. Updated version.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/psygenet2r@116447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/disGeNET.R
+++ R/disGeNET.R
@@ -1,64 +1,67 @@
 disgenetAll <- function( database, verbose = FALSE ) {
-  if( verbose ) {
-    message( "Staring querying DisGeNET in '", database , "' database." )
-  }
-  
-  oql <- "DEFINE
-  c0='/data/gene_to_associated_diseases',
-  c1='/data/genes',
-  c2='/data/genes_to_gene_number',
-  c3='/data/sources'
-  ON
-  'http://bitbucket.org/janis_pi/disgenet_onexus.git'
-  
-  SELECT
-  c1 (name, geneId, description ),
-  c0 (numberOfassocDiseases, sourceId),
-  c2 (geneId, NoAssocGenes, sourceId, geneId, sourceId)
-  FROM
-  c0
-  WHERE
-  c3 = 'DB'
-  ORDER BY
-  c0.numberOfassocDiseases DESC" 
-
-  oql <- stringr::str_replace(
-      string      = oql,
-      pattern     = "DB",
-      replacement = database 
-  )
-
-  dataTsv <- RCurl::getURLContent(
-    getUrlDis(), 
-    readfunction  = charToRaw(oql), 
-    upload        = TRUE, 
-    customrequest = "POST"
+    if( verbose ) {
+        message( "Staring querying DisGeNET in '", database , "' database." )
+    }
+    
+    oql <- "DEFINE
+    c0='/data/gene_to_associated_diseases',
+    c1='/data/genes',
+    c2='/data/gene_roles',
+    c3='/data/gene_to_gene_number',
+    c4='/data/sources'
+    ON
+    'http://www.disgenet.org/web/DisGeNET'
+    
+    SELECT
+    c1 (name, geneId, description ),
+    c0 (numberOfassocDiseases, sourceId),
+    c3 (geneId, NoAssocGenes, sourceId, geneId, sourceId)
+    FROM
+    c0
+    WHERE
+    c4 = 'DB'
+    ORDER BY
+    c0.numberOfassocDiseases DESC" 
+    
+    
+    oql <- stringr::str_replace(
+        string      = oql,
+        pattern     = "DB",
+        replacement = database 
+    )
+    
+    dataTsv <- RCurl::getURLContent(
+        getUrlDis(), 
+        readfunction  = charToRaw(oql), 
+        upload        = TRUE, 
+        customrequest = "POST"
     ) 
-  data <- read.csv(textConnection(dataTsv), header = TRUE, sep="\t")
-
-  return( data )
+    data <- read.csv(textConnection(dataTsv), header = TRUE, sep="\t")
+    
+    return( data )
 }
 
 ###############################################################################
 
 disGenetCurated <- function ( database ) {
-  oql <- "DEFINE
+    oql <- "DEFINE
     c0='/data/gene_to_associated_diseases',
-  c1='/data/genes',
-  c2='/data/genes_to_gene_number',
-  c3='/data/sources'
-  ON
-  'http://bitbucket.org/janis_pi/disgenet_onexus.git'
-  SELECT
-  c1 (geneId, uniprotId),
-  c0 (numberOfassocDiseases),
-  c2 (sourceId)
-  FROM
-  c0
-  WHERE
-  c3 = 'DB'
-  ORDER BY
-  c0.numberOfassocDiseases DESC" 
+    c1='/data/genes',
+    c2='/data/gene_roles',
+    c3='/data/gene_to_gene_number',
+    c4='/data/sources'
+    ON
+    'http://www.disgenet.org/web/DisGeNET'
+    SELECT
+    c1 (geneId, uniprotId),
+    c0 (numberOfassocDiseases),
+    c3 (sourceId)
+    FROM
+    c0
+    WHERE
+    c4 = 'DB'
+    ORDER BY
+    c0.numberOfassocDiseases DESC" 
       
   oql <- stringr::str_replace(
       string      = oql,
