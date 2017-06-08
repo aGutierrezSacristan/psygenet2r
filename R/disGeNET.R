d8e85e2ce150cf1561344b6023b52381d15e2a94
Gutierrez <ALBA_GUTIERREZ@HMS.HARVARD.EDU>
2017-06-08 17:25:24 -0400

    jaccard function adapted to the new Disgenet version

--- R/disGeNET.R
+++ R/disGeNET.R
@@ -1,62 +1,22 @@
-disgenetAll <- function( database, verbose = FALSE ) {
-    if( verbose ) {
-        message( "Staring querying DisGeNET in '", database , "' database." )
-    }
-    
-    oql <- "DEFINE
-    c0='/data/gene_to_associated_diseases',
-    c1='/data/genes',
-    c2='/data/gene_roles',
-    c3='/data/gene_to_gene_number',
-    c4='/data/sources'
-    ON
-    'http://www.disgenet.org/web/DisGeNET'
-    
-    SELECT
-    c1 (name, geneId, description ),
-    c0 (numberOfassocDiseases, sourceId),
-    c3 (geneId, NoAssocGenes, sourceId, geneId, sourceId)
-    FROM
-    c0
-    WHERE
-    c4 = 'DB'
-    ORDER BY
-    c0.numberOfassocDiseases DESC" 
-    
-    
-    oql <- stringr::str_replace(
-        string      = oql,
-        pattern     = "DB",
-        replacement = database 
-    )
-    
-    dataTsv <- download_data(oql, url = getUrlDis())
-    data <- read.csv(textConnection(dataTsv), header = TRUE, sep="\t")
-    
-    return( data )
-}
-
-###############################################################################
+##############################################################################
 
 disGenetCurated <- function ( database ) {
+
     oql <- "DEFINE
-    c0='/data/gene_to_associated_diseases',
+	c0='/data/gene_to_associated_diseases',
     c1='/data/genes',
-    c2='/data/gene_roles',
-    c3='/data/gene_to_gene_number',
-    c4='/data/sources'
+    c2='/data/sources'
     ON
     'http://www.disgenet.org/web/DisGeNET'
     SELECT
-    c1 (geneId, uniprotId),
-    c0 (numberOfassocDiseases),
-    c3 (sourceId)
+    c1 (symbol, geneId),
+    c0 ( Ndiseases, source)
     FROM
     c0
     WHERE
-    c4 = 'DB'
+    c2 = 'ALL'
     ORDER BY
-    c0.numberOfassocDiseases DESC" 
+    c0.Ndiseases DESC" 
       
   oql <- stringr::str_replace(
       string      = oql,
