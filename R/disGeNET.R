4cc6eb77a690e38067a95f6c5c89fd9bb73ecd66
albags <>
2017-06-10 12:16:49 +0200

    Solved problem in disGenetCurated.

--- R/disGeNET.R
+++ R/disGeNET.R
@@ -1,39 +1,31 @@
 ##############################################################################
 
-disGenetCurated <- function ( database ) {
+disGenetCurated <- function (  ) {
 
     oql <- "DEFINE
-	c0='/data/gene_to_associated_diseases',
-    c1='/data/genes',
-    c2='/data/sources'
-    ON
-    'http://www.disgenet.org/web/DisGeNET'
-    SELECT
-    c1 (symbol, geneId),
-    c0 ( Ndiseases, source)
-    FROM
-    c0
-    WHERE
-    c2 = 'ALL'
-    ORDER BY
-    c0.Ndiseases DESC" 
-      
-  oql <- stringr::str_replace(
-      string      = oql,
-      pattern     = "DB",
-      replacement = database 
-  )
+    	c0='/data/gene_to_associated_diseases',
+        c1='/data/genes',
+        c2='/data/sources'
+        ON
+        'http://www.disgenet.org/web/DisGeNET'
+        SELECT
+        c1 (symbol, geneId),
+        c0 ( Ndiseases, source)
+        FROM
+        c0
+        WHERE
+        c2 = 'ALL'
+        ORDER BY
+        c0.Ndiseases DESC"
   
-  dataTsv <- RCurl::getURLContent ( 
-    getUrlDis(), 
-    readfunction  = charToRaw( oql ), 
-    upload        = TRUE, 
-    customrequest = "POST" 
-  )
-  data <- read.csv ( textConnection( dataTsv ), header = TRUE, sep="\t" )  
-  proteinUniverse <- unique ( data[,1] )
-  return ( proteinUniverse )
-  
-    
+    dataTsv <- RCurl::getURLContent ( 
+        psygenet2r:::getUrlDis(), 
+        readfunction  = charToRaw( oql ), 
+        upload        = TRUE, 
+        customrequest = "POST" 
+    )
+    data <- read.csv ( textConnection( dataTsv ), header = TRUE, sep = "\t" )  
+    proteinUniverse <- unique ( data[ , 1 ] )
+    return ( proteinUniverse )
 }
 
